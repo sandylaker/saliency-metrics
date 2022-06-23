b1d3cfe4768aefb1a409a92e340255c59af74d5e
rjagtani <rjagtani@gmail.com>
2022-06-23 17:26:38 +0200

    Added function and static type checking

--- saliency_metrics/metrics/insertion_deletion/insertion_deletion.py
+++ saliency_metrics/metrics/insertion_deletion/insertion_deletion.py
@@ -1,41 +1,45 @@
-# Build Dataset and Load Smap
+from typing import Dict, List
+
 import torch
 from insertion_deletion_metric import InsertionDeletion
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-# TODO Discuss config,datasets and dataloader
-training_data = datasets.CIFAR10(root="data", train=True, download=True, transform=ToTensor())
 
-# Build dataloader
-batch_size = 16
-train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-batch = {}
-for data in train_dataloader:
-    batch["img"] = data[0]
-    batch["targets"] = data[1]
-    smaps = []
-    for img in data[0]:
-        smap = torch.mean(img, axis=0)
-        smaps.append(smap)
-    batch["smaps"] = torch.stack(smaps)
-    break
+def run_insertion_deletion() -> None:
+    training_data = datasets.CIFAR10(root="data", train=True, download=True, transform=ToTensor())
 
-# Classifier Config
-classifier_config = dict(type="torchvision.resnet18", num_classes=10, pretrained=False)
+    # Build dataloader
+    batch_size = 16
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    batch: Dict[str, torch.tensor] = {}
+    for data in train_dataloader:
+        batch["img"] = data[0]
+        batch["targets"] = data[1]
+        smaps: List[torch.tensor] = []
+        for img in data[0]:
+            smap = torch.mean(img, axis=0)
+            smaps.append(smap)
+        batch["smaps"] = torch.stack(smaps)
+        break
 
-# Instantiate Class
-ins_del = InsertionDeletion(classifier_config, forward_batch_size=32, perturb_step_size=10, summarized=False)
-file_path = r"saliency_metrics\metrics\insertion_deletion\results.json"
-images = []
-smaps = []
-for i in range(2):
-    img = batch["img"][i]
-    smap = batch["smaps"][i]
-    target = batch["targets"][i].item()
-    single_result = ins_del.evaluate(img, smap, target)
-    ins_del.update(single_result)
-# TODO Discuss dump
-result = ins_del.get_result
-result.dump(file_path)
+    # Classifier Config
+    classifier_config = dict(type="torchvision.resnet18", num_classes=10, pretrained=False)
+
+    # Instantiate Class
+    ins_del = InsertionDeletion(classifier_config, forward_batch_size=32, perturb_step_size=10, summarized=True)
+    file_path = r"saliency_metrics\metrics\insertion_deletion\results.json"
+    for i in range(15):
+        img = batch["img"][i]
+        smap = batch["smaps"][i]
+        target = batch["targets"][i].item()
+        img_path = r"user\somepath"
+        single_result = ins_del.evaluate(img, smap, target, img_path)
+        ins_del.update(single_result)
+    result = ins_del.get_result
+    result.dump(file_path)
+
+
+if __name__ == "__main__":
+    run_insertion_deletion()
