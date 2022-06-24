f2bf9bc7d9c5aebbcba8d7811eb5101a6d88a1f1
rjagtani <rjagtani@gmail.com>
2022-06-24 15:16:54 +0200

    added run_insertion_deletion

--- saliency_metrics/metrics/insertion_deletion/__init__.py
+++ saliency_metrics/metrics/insertion_deletion/__init__.py
@@ -1,5 +1,6 @@
+from .insertion_deletion import run_insertion_deletion
 from .insertion_deletion_metric import InsertionDeletion
 from .insertion_deletion_perturbation import ProgressivePerturbation
 from .insertion_deletion_result import InsertionDeletionResult
 
-__all__ = ["InsertionDeletionResult", "ProgressivePerturbation", "InsertionDeletion"]
+__all__ = ["InsertionDeletionResult", "ProgressivePerturbation", "InsertionDeletion", "run_insertion_deletion"]
