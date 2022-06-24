698edf1e54e8bf19db73df03cb4321b2376c8add
rjagtani <rjagtani@gmail.com>
2022-06-24 15:28:23 +0200

    Added fixed image and smap

--- saliency_metrics/metrics/insertion_deletion/insertion_deletion.py
+++ saliency_metrics/metrics/insertion_deletion/insertion_deletion.py
@@ -1,10 +1,11 @@
 from typing import Dict, List
 
 import torch
-from insertion_deletion_metric import InsertionDeletion
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
+from saliency_metrics.metrics.insertion_deletion import InsertionDeletion
 
 
 def run_insertion_deletion() -> None:
@@ -28,12 +29,16 @@ def run_insertion_deletion() -> None:
     classifier_config = dict(type="torchvision.resnet18", num_classes=10, pretrained=False)
 
     # Instantiate Class
-    ins_del = InsertionDeletion(classifier_config, forward_batch_size=32, perturb_step_size=10, summarized=True)
+    ins_del = InsertionDeletion(classifier_config, forward_batch_size=32, perturb_step_size=10, summarized=False)
+    # TODO - change file path
     file_path = r"saliency_metrics\metrics\insertion_deletion\results.json"
-    for i in range(15):
-        img = batch["img"][i]
-        smap = batch["smaps"][i]
-        target = batch["targets"][i].item()
+    for i in range(1):
+        # img = batch["img"][i]
+        # smap = batch["smaps"][i]
+        # target = batch["targets"][i].item()
+        img = torch.linspace(0, 1, 3072, dtype=torch.float32).reshape(3, 32, 32)
+        smap = torch.linspace(0, 1, 1024, dtype=torch.float32).reshape(32, 32)
+        target = 3
         img_path = r"user\somepath"
         single_result = ins_del.evaluate(img, smap, target, img_path)
         ins_del.update(single_result)
