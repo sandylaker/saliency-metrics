a4daef6244b6e5c25b2088979fb20bb178b02bd4
rjagtani <rjagtani@gmail.com>
2022-07-06 18:34:35 +0200

    changed name from array to list

--- saliency_metrics/metrics/insertion_deletion/insertion_deletion_result.py
+++ saliency_metrics/metrics/insertion_deletion/insertion_deletion_result.py
@@ -21,17 +21,17 @@ class InsertionDeletionResult(SerializableResult):
 
     def dump(self, file_path: str) -> None:
         if self.summarized:
-            del_auc_array: List[float] = []
-            ins_auc_array: List[float] = []
+            del_auc_scores_list: List[float] = []
+            ins_auc_scores_list: List[float] = []
             for result in self.results:
-                del_auc_array.append(result["del_auc"])
-                ins_auc_array.append(result["ins_auc"])
+                del_auc_scores_list.append(result["del_auc"])
+                ins_auc_scores_list.append(result["ins_auc"])
                 summarized_result = {
-                    "mean_insertion_auc": np.mean(ins_auc_array),
-                    "std_insertion_auc": np.std(ins_auc_array),
-                    "mean_deletion_auc": np.mean(del_auc_array),
-                    "std_deletion_auc": np.std(del_auc_array),
-                    "num_samples": len(del_auc_array),
+                    "mean_insertion_auc": np.mean(ins_auc_scores_list),
+                    "std_insertion_auc": np.std(ins_auc_scores_list),
+                    "mean_deletion_auc": np.mean(del_auc_scores_list),
+                    "std_deletion_auc": np.std(del_auc_scores_list),
+                    "num_samples": len(del_auc_scores_list),
                 }
             mmcv.mkdir_or_exist(osp.dirname(file_path))
             mmcv.dump(summarized_result, file_path)
