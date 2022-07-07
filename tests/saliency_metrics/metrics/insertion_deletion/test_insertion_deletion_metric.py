bd8a106b42712005b584d79c2fcfac40131ec189
rjagtani <rjagtani@gmail.com>
2022-07-07 11:56:41 +0200

    changes for 4d image

--- tests/saliency_metrics/metrics/insertion_deletion/test_insertion_deletion_metric.py
+++ tests/saliency_metrics/metrics/insertion_deletion/test_insertion_deletion_metric.py
@@ -35,7 +35,7 @@ class TestNet(nn.Module):
 @pytest.fixture
 def dummy_img_and_smap():
     # three-channel image
-    img = torch.linspace(0, 1, 3072, dtype=torch.float32).reshape(3, 32, 32)
+    img = torch.linspace(0, 1, 3072, dtype=torch.float32).reshape(1, 3, 32, 32)
     # saliency map
     smap = torch.linspace(0, 1, 1024, dtype=torch.float32).reshape(32, 32)
     yield img, smap
@@ -43,10 +43,27 @@ def dummy_img_and_smap():
 
 @pytest.mark.parametrize("perturb_step_size", [250, 1025])
 @pytest.mark.parametrize("forward_batch_size", [0, 32])
-def test_evaluate(dummy_img_and_smap, perturb_step_size, forward_batch_size):
+def test_init(perturb_step_size, forward_batch_size):
+    classifier_cfg = dict(type="custom.TestNet")
+    if forward_batch_size == 0 or perturb_step_size == 0:
+        with pytest.raises(ValueError):
+            ins_del = InsertionDeletion(
+                classifier_cfg, forward_batch_size=forward_batch_size, perturb_step_size=perturb_step_size
+            )
+    else:
+        ins_del = InsertionDeletion(
+            classifier_cfg, forward_batch_size=forward_batch_size, perturb_step_size=perturb_step_size
+        )
+        assert ins_del.forward_batch_size == forward_batch_size
+        assert ins_del.perturb_step_size == perturb_step_size
+
+
+@pytest.mark.parametrize("perturb_step_size", [250, 1025])
+def test_evaluate(dummy_img_and_smap, perturb_step_size):
     img, smap = dummy_img_and_smap
     target = 3
     classifier_cfg = dict(type="custom.TestNet")
+    forward_batch_size = 32
     ins_del = InsertionDeletion(
         classifier_cfg, forward_batch_size=forward_batch_size, perturb_step_size=perturb_step_size, summarized=False
     )
@@ -54,39 +71,35 @@ def test_evaluate(dummy_img_and_smap, perturb_step_size, forward_batch_size):
         with pytest.raises(ValueError, match="perturb_step_size should be"):
             _ = ins_del.evaluate(img, smap, target, img_path="user/somepath")
     else:
-        if forward_batch_size == 0:
-            with pytest.raises(ValueError, match="forward_batch_size should be"):
-                _ = ins_del.evaluate(img, smap, target, img_path="user/somepath")
-        else:
-            single_result = ins_del.evaluate(img, smap, target, img_path="user/somepath")
-            expected_result = {
-                "del_scores": [
-                    0.09772268682718277,
-                    0.09906678646802902,
-                    0.10010480135679245,
-                    0.10033050924539566,
-                    0.09997809678316116,
-                ],
-                "ins_scores": [
-                    0.09874380379915237,
-                    0.09874299168586731,
-                    0.0987432524561882,
-                    0.0987226590514183,
-                    0.0987049862742424,
-                ],
-                "img_path": "user/somepath",
-                "del_auc": 0.0796705037355423,
-                "ins_auc": 0.07898665964603424,
-            }
-            assert single_result["ins_auc"] == pytest.approx(expected_result["ins_auc"], 1e-4)
-            assert single_result["del_auc"] == pytest.approx(expected_result["del_auc"], 1e-4)
-            assert len(single_result["del_scores"]) == len(expected_result["del_scores"])
-            assert len(single_result["ins_scores"]) == len(expected_result["ins_scores"])
-            assert single_result["img_path"] == expected_result["img_path"]
-            ins_del.update(single_result)
-            test_result = ins_del.get_result
-            assert test_result.results[0]["ins_auc"] == pytest.approx(expected_result["ins_auc"], 1e-4)
-            assert test_result.results[0]["del_auc"] == pytest.approx(expected_result["del_auc"], 1e-4)
-            assert len(test_result.results[0]["del_scores"]) == len(expected_result["del_scores"])
-            assert len(test_result.results[0]["ins_scores"]) == len(expected_result["ins_scores"])
-            assert test_result.results[0]["img_path"] == expected_result["img_path"]
+        single_result = ins_del.evaluate(img, smap, target, img_path="user/somepath")
+        expected_result = {
+            "del_scores": [
+                0.09772268682718277,
+                0.09906678646802902,
+                0.10010480135679245,
+                0.10033050924539566,
+                0.09997809678316116,
+            ],
+            "ins_scores": [
+                0.09874380379915237,
+                0.09874299168586731,
+                0.0987432524561882,
+                0.0987226590514183,
+                0.0987049862742424,
+            ],
+            "img_path": "user/somepath",
+            "del_auc": 0.0796705037355423,
+            "ins_auc": 0.07898665964603424,
+        }
+        assert single_result["ins_auc"] == pytest.approx(expected_result["ins_auc"], 1e-4)
+        assert single_result["del_auc"] == pytest.approx(expected_result["del_auc"], 1e-4)
+        assert len(single_result["del_scores"]) == len(expected_result["del_scores"])
+        assert len(single_result["ins_scores"]) == len(expected_result["ins_scores"])
+        assert single_result["img_path"] == expected_result["img_path"]
+        ins_del.update(single_result)
+        test_result = ins_del.get_result
+        assert test_result.results[0]["ins_auc"] == pytest.approx(expected_result["ins_auc"], 1e-4)
+        assert test_result.results[0]["del_auc"] == pytest.approx(expected_result["del_auc"], 1e-4)
+        assert len(test_result.results[0]["del_scores"]) == len(expected_result["del_scores"])
+        assert len(test_result.results[0]["ins_scores"]) == len(expected_result["ins_scores"])
+        assert test_result.results[0]["img_path"] == expected_result["img_path"]
